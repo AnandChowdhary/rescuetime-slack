15bff4d908f66908fa4cd738d492411244020570
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-26 13:59:06 +0530

    :construction: Fetch RescueTime summary

--- mod.ts
+++ mod.ts
@@ -1,4 +1,32 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
-}
+import { load } from "https://deno.land/x/js_yaml_port/js-yaml.js";
+import { join } from "https://deno.land/std@0.51.0/path/mod.ts";
+import { readFileStr } from "https://deno.land/std@0.51.0/fs/mod.ts";
+
+/** Fetch RescueTime daily summary for user */
+export const fetchDailySummary = async (
+  apiKey: string
+): Promise<{
+  //
+}> => {
+  const response = await fetch(
+    `https://www.rescuetime.com/anapi/daily_summary_feed?key=${apiKey}`
+  );
+  if (!response.ok) throw new Error("Unable to fetch");
+  return response.json();
+};
+
+export const postToSlack = async (message: string) => {
+  console.log(message);
+};
+
+export const rescuetimeSlack = async () => {
+  console.log("Running");
+  const file = await readFileStr(join(".", "rescuetime-slack.yml"));
+  const config: { apiKeys: string[] } = load(file);
+  for await (const key of config.apiKeys) {
+    const summary = await fetchDailySummary(key);
+    console.log(summary);
+  }
+};
+
+rescuetimeSlack();
