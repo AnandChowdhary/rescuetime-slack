8bdd2ba4fb05dcaacbee90ffa7f9f1ffb3e47fe3
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-26 15:19:28 +0530

    :recycle: Use bot name and image

--- mod.ts
+++ mod.ts
@@ -40,6 +40,9 @@ export const postToSlack = async (
   data: RescueTimeDailySummary
 ) => {
   const payload = {
+    username: "Koj Bot",
+    icon_url:
+      "https://raw.githubusercontent.com/AnandChowdhary/rescuetime-slack/master/assets/bot.png",
     text: `*RescueTime* summary for ${user} for ${data.date}`,
     blocks: [
       {
