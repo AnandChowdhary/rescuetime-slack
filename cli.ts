5f4008eea34e4dfd6bcea74e9d2bcb18deef8461
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-26 13:49:51 +0530

    :fire: Remove CLI

--- cli.ts
+++
@@ -1,8 +0,0 @@
-import { mode } from "./mod.ts";
-
-// https://deno.land/manual/tools/script_installer
-if (import.meta.main) {
-  for (let arg of Deno.args) {
-    console.log(arg, mode());
-  }
-}
