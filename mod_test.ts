48144d07d549bf7e01e2b9b933f9c3da2b0aaa30
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-26 15:18:07 +0530

    :construction_worker: Add GitHub Actions CI

--- mod_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { mode } from "./mod.ts";
-
-Deno.test("test starter function", async (): Promise<void> => {
-  assertEquals(mode(), 0);
-});
